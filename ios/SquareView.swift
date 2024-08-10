b92f385db513aaedf1b022ba08b5605c521a34db
Hoang Nguyen Huy <hoangnh0099@gmail.com>
2024-08-10 11:56:12 +0700

    feat: update

--- ios/SquareView.swift
+++ ios/SquareView.swift
@@ -1,16 +1,11 @@
 import SwiftUI
 
 struct SquareView: View {
-    var width: CGFloat
-    var height: CGFloat
-
     var body: some View {
         VStack {
             Text("Haha")
-                .frame(width: width, height: height)
+                .frame(width: 100, height: 100)
                 .background(.red)
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                .cornerRadius(8)
         }
     }
 }
--- ios/SquareViewManager.swift
+++ ios/SquareViewManager.swift
@@ -3,22 +3,13 @@ import SwiftUI
 
 @objc(SquareViewManager)
 class SquareViewManager: RCTViewManager {
-  private var width: CGFloat = 100
-  private var height: CGFloat = 100
   
-  func view() -> SquareView! {
-    return SquareView(width: width, height: height)
+  override func view() -> UIView! {
+    let hostingController = UIHostingController(rootView: SquareView())
+    return hostingController.view
   }
   
   override static func requiresMainQueueSetup() -> Bool {
     return true
-  }
-  
-  @objc func setWidth(_ node: NSNumber, width: CGFloat) {
-    self.width = width
-  }
-  
-  @objc func setHeight(_ node: NSNumber, height: CGFloat) {
-    self.height = height
   }
 }
